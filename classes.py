049cf5f7da31df883ab966dbd664959fdf509401
Mtvy <mtvy@gmail.com>
2022-01-06 03:20:29 +0300

    wallet and balance class add

--- classes.py
+++ classes.py
@@ -8,7 +8,7 @@ class Account:
     This class creates accounts with all user data.
     Need array acc that includes twelve params.
     """
-    def __init__(self, *args) -> None:
+    def __init__(self, *args):
         
         (self.user_id    ,
          self.username   ,
@@ -20,6 +20,7 @@ class Account:
          self.wallet_id  ) = args
     
     def __str__(self) -> str:
+        
         return f'Account data ->                      \n\
                      user_id    : { self.user_id    } \n\
                      username   : { self.username   } \n\
@@ -35,7 +36,8 @@ class DB_Access:
     """
     This class collect data for database connection.
     """
-    def __init__(self, *args) -> None:
+    def __init__(self, *args):
+        
         self.database, self.user, self.password, self.host, self.port = args
     
     def __str__(self):
@@ -49,48 +51,80 @@ class DB_Access:
 
 
 class Balance:
-    
-    def __init__(self, *args) -> int:
+    """
+    This object include balance of the account.
+    """ 
+    def __init__(self, *args):
         
-        (self.usd ,
-         self.rub ,
-         self.euro,
-         self.usdt,
-         self.btc ,
-         self.eth ,
-         self.sol ) = args
+        (self.usd , self.rub , self.euro,
+         self.usdt, self.btc , self.eth ,
+         self.sol                       ) = args
          
-        return generate_balance_id()
+        self.balance_id   = self.generate_balance_id()
+        self.balance_hash = None # generate_hash()
         
+    
     def generate_balance_id(self) -> int:
+    	
         self.balance_id = 0
+        
         return self.balance_id
 
 
-class Wallet:
-    
-    
-    def __init__(self, account : Account, *args) -> int:
+class Wallet(Balance):
+    """
+    This object include info about wallet and balance.
+    """
+    def __init__(self, user_info : (int, str), *args):
         
+        super().__init__(*args)
+        self.user_id, self.status = user_info
         
-        self.user_id, self.status = account.user_id, account.status
+        self.wallet_id   = self.generate_wallet_id()
+        self.wallet_hash = None # generate_hash()
         
-        self.hash = None
-        
-        self.balance_id = Balance(*args)
-         
-        return generate_wallet_id()
     
     def generate_wallet_id(self) -> int:
+        
         self.wallet_id = 0
+        
         return self.wallet_id
+    
+    def __str__(self) -> str:
+        
+        return f'Wallet info ->                           \n\
+                 user_id    : { self.user_id    }         \n\
+                 status     : { self.status     }         \n\
+                 wallet_id  : { self.wallet_id  }         \n\
+                 balance_id : { self.balance_id }         \n\
+                                                          \n\
+                 Balance info ->                          \n\
+                                   usd  : { self.usd  }   \n\
+                                   rub  : { self.rub  }   \n\
+                                   euro : { self.euro }   \n\
+                                   usdt : { self.usdt }   \n\
+                                   btc  : { self.btc  }   \n\
+                                   eth  : { self.eth  }   \n\
+                                   sol  : { self.sol  } \n\n\
+                 wallet_hash  = { self.wallet_hash  }     \n\
+                 balance_hash = { self.balance_hash }     \n'
+
+
+
+
 
 if __name__ == '__main__':
 	
-    account   = Account('8888', 'mtvy', 'None', 'None', 'None', '04.01.22 02:20:22', 'None', 'C0DE000000000000')
+    account = Account('8888', 'mtvy', 'None', 'None', 'None', '04.01.22 02:20:22', 'None', 'C0DE000000000000')
     
     print(account)
     
     db_access = DB_Access(None, None, None, None, None)
     
     print(db_access)
+    
+    value  = (0, 0, 0, 0, 0, 0, 0)
+    
+    wallet = Wallet((account.user_id, account.status), *value)
+    
+    print(wallet)
